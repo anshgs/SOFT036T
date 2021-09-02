d4dd2a02b826f89f38350420b7ea37f6152d7cc3
Ansh Sharma <40280081+anshgs@users.noreply.github.com>
2021-09-01 23:56:58 -0500

    Update EntanglementScheme.py

--- EntanglementScheme.py
+++ EntanglementScheme.py
@@ -4,161 +4,201 @@ from numpy.random import randint
 import numpy as np
 # print("Imports Successful")
 
-print("BITS TO ENTANGLE") #A+B ALREADY KNOW THIS (protocol)
-b1 = int(input("Bit 1: "))
-b2 = int(input("Bit 2: "))
-entangled = (b1, b2)
+n = 10
 
-def encode_message(bits, bases):
-    message = []
+entangled_sets = [[0, 2],[7,9]] #A+B ALREADY KNOW THIS (protocol)
+print("e1: q0")
+print("e2: q2")
+
+
+def encode_message(qc, bits, bases):
     for i in range(n):
-        qc = QuantumCircuit(1,1)
         if bases[i] == 0: # Prepare qubit in Z-basis
             if bits[i] == 0:
                 pass # |0>
             else:
-                qc.x(0) # |1>
+                qc.x(i) # |1>
         else: # Prepare qubit in X-basis
             if bits[i] == 0:
-                qc.h(0) # |+>
+                qc.h(i) # |+>
             else:
-                qc.x(0)
-                qc.h(0) # |->
-        qc.barrier()
-        message.append(qc)
-    return message
+                qc.x(i)
+                qc.h(i) # |->
+    return qc
 
-def simple_entangle(e1, e2, arr):
+def simple_entangle(qc, entangling):
+    #correlated entanglement Bell-Pair Producer
+    qc.h(entangling[0])
+    qc.cx(entangling[0], entangling[1])
 
-    #method simple_entangle entangles qubits with H-CX bell-pair producer
-    #measurements of entangled qubits are assumed to be in the simple Z axis
-    #added functionality (i.e. inverse entanglement, entanglement in X basis) will added in later
+    return qc
 
-    eqc = QuantumCircuit(2,2)
-
-    eqc.h(0)
-    eqc.cx(0,1)
-    eqc.barrier()
-
-    del arr[e1]
-    arr.insert(e1, eqc)
-    #the actual message cannot be sent with the "placeholder" message
-    #then, eve would know that bit is entangled with another
-    #so, position of secondary entangled bit e2 is held constant (indistinguishable)
-
-    return arr
-
-def measure_message(message, bases):
+def measure_message(qc, bases):
     backend = Aer.get_backend('qasm_simulator')
     measurements = [None]*n #create measurement list
-    entangle_done = False
 
-    for q in range(n):
-        if (entangle_done == False) or (q != entangled[1]):
+    for j in range(n):
+        if bases[j] == 0: # measuring in Z-basis
+            qc.measure(j,j)
+        if bases[j] == 1: # measuring in X-basis
+            qc.h(j)
+            qc.measure(j,j)
+    result = execute(qc, backend, shots=1, memory=True).result()
+    counts = result.get_counts(qc)
+    sequence = str(next(iter(counts)))
 
-            if len(message[q].qubits) == 2: #if the state is entangled
-
-                #Bob identifies which entangled bit he has encountered:
-                if q == entangled[0]:
-                    print("Primary Entangled Bit Found")
-                    other = entangled[1]
-                else:
-                    other = entangled[0]
-
-                if bases[q] == 0: # TEMP: measuring in Z-basis
-                    message[q].measure(0,0)
-                    message[q].measure(1,1)
-                result = execute(message[q], backend, shots=1, memory=True).result()
-                counts = result.get_counts(message[q])
-                primary_bit = next(iter(counts))[0] #[0] ensures that only measurement of ONE qubit is added to the result
-                primary_bit = primary_bit + "*" #TEMP
-                measurements[q] = primary_bit
-
-                #you've added in the primary entangled bit, now replace or add in the second one
-                secondary_bit = next(iter(counts))[1] #[1] ensures that only measurement of SECOND qubit is added to the result
-                secondary_bit = secondary_bit + "*" #TEMP
-                measurements[other] = secondary_bit
-                entangle_done = True
-
-            else: #normal bits
-                if bases[q] == 0: # measuring in Z-basis
-                    message[q].measure(0,0)
-                if bases[q] == 1: # measuring in X-basis
-                    message[q].h(0)
-                    message[q].measure(0,0)
-                result = execute(message[q], backend, shots=1, memory=True).result()
-                counts = result.get_counts(message[q])
-                measured_bit = next(iter(counts))
-                measurements[q] = measured_bit
-        else:
-            entangle_done = False #resets as if entanglement didn't happen
+    for k in range(n):
+        measurements[k] = str(sequence[n-k-1])
 
     return measurements
 
-def remove_garbage(a_bases, b_bases, bits):
-    good_bits = []
+def prep_entangle(alice_bits, alice_bases, entangling):
+    #TEMP: Adjust for Alice polarizer to encode in simple Z basis
+    #Bob measures in any basis. if incompatible, discarded. If both compatible, both will be correlated
+    #if only one is compatible, the other will be appended
+    alice_bases[entangling[0]] = 0
+    alice_bases[entangling[1]] = 0
+    alice_bits[entangling[0]] = 0
+    alice_bits[entangling[1]] = 0
+
+def remove_garbage(a_bases, b_bases, bits, entangling):
+    output = [None]*n #create measurement list
     for q in range(n):
-        if a_bases[q] == b_bases[q]:
-            # If both used the same basis, add
-            # this to the list of 'good' bits
-            good_bits.append(bits[q])
-    return good_bits
+        if a_bases[q] != b_bases[q]:
+            # If both did not use same basis,
+            # change value to None
+            #unless one of them is entangled
+            if q == entangling[0]:
+                print("e1 eliminated")
+                output[q] = False
+            elif q == entangling[1]:
+                print("e2 eliminated")
+                output[q] = False
+            else:
+                output[q] = None
+
+        else:
+            output[q] = bits[q]
+
+    return output
+
+def adjust_entangle(bits, entangling, bob_value):
+
+    if (bits[entangling[0]] == False and bits[entangling[1]] != False):
+        if (bits[entangling[1]] == "?*"):
+            bits[entangling[1]] = bob_value
+            bits.append(bob_value)
+        else:
+            bits.append(bits[entangling[1]])
+        bits.pop(entangling[0])
+
+    if (bits[entangling[1]] == False and bits[entangling[0]] != False):
+        if (bits[entangling[0]] == "?*"):
+            bits[entangling[0]] = bob_value
+            bits.append(bob_value)
+        else:
+            bits.append(bits[entangling[0]])
+        bits.pop(entangling[1])
+
+    return bits
+
+def clear_nones(sequence):
+    output = []
+    for q in range(len(sequence)):
+        if sequence[q] == None or sequence[q] == False:
+            pass
+        else:
+            output.append(sequence[q])
+
+    return output
+
+def bb84(n, entangled_bits):
+
+    qc = QuantumCircuit(n,n)
+
+    ## Step 1
+    # Alice generates bits
+    alice_bits = randint(2, size=n)
 
 
-n = 20
+    ## Step 2
+    # Create an array to tell us which qubits
+    # are encoded in which bases
+    alice_bases = randint(2, size=n)
 
-## Step 1
-# Alice generates bits
-alice_bits = randint(2, size=n)
+    prep_entangle(alice_bits, alice_bases, entangled_bits)
+    message = encode_message(qc, alice_bits, alice_bases)
 
-## Step 2
-# Create an array to tell us which qubits
-# are encoded in which bases
-alice_bases = randint(2, size=n)
-message = encode_message(alice_bits, alice_bases)
+    entangled = simple_entangle(message, entangled_bits)
 
-print("BEFORE ENTANGLEMENT:")
+    ## Step 3
+    # Decide which basis to measure in:
+    bob_bases = randint(2, size=n)
 
-print("Base: " + str(alice_bases[b1]) + "; Bit: " + str(alice_bits[b1]))
-print(message[b1]);
+    bob_results = measure_message(entangled, bob_bases)
 
-print()
+    #TEMP: Add in * to designate entangled bits
+    bob_results[entangled_bits[0]] = str(bob_results[entangled_bits[0]])+"*"
+    bob_results[entangled_bits[1]] = str(bob_results[entangled_bits[1]])+"*"
 
-print("Base: " + str(alice_bases[b2]) + "; Bit: " + str(alice_bits[b2]))
-print(message[b2]);
+    new_alice_bits = []
+    for i in range(0, len(alice_bits)):
+        if i == entangled_bits[0] or i == entangled_bits[1]:
+            new_alice_bits.append("?*")
+        else:
+            new_alice_bits.append(str(alice_bits[i]))
 
-print()
-print("AFTER ENTANGLEMENT:")
-res = simple_entangle(b1, b2, message)
-print(res[b1])
-print(res[b2])
+    print("Alice bases:")
+    print(alice_bases)
+    print("Bob bases:")
+    print(bob_bases)
 
-## Step 3
-# Decide which basis to measure in:
-bob_bases = randint(2, size=n)
+    print()
+    print("INITIALIZATION (Alice):")
+    print(new_alice_bits)
 
-#TEMP: Adjust for Bob polarizer to measure in simple Z basis
-bob_bases[b1] = 0
-bob_bases[b2] = 0
+    print()
+    print("MEASUREMENT (Bob):")
+    print("Bob unfiltered:")
+    print(bob_results)
 
-bob_results = measure_message(message, bob_bases)
+    print("n: " + str(len(bob_results)))
 
-print("Bob unfiltered:")
-print(bob_results)
+    # Step 4
+    print()
+    print("RESULTS:")
 
-print("Length (should be n): " + str(len(bob_results)))
+    alice_uncleared = remove_garbage(alice_bases, bob_bases, new_alice_bits, entangled_bits)
+    print("Alice uncleared:")
+    print(alice_uncleared)
+    print()
+    bob_uncleared = remove_garbage(alice_bases, bob_bases, bob_results, entangled_bits)
+    print("Bob uncleared:")
+    print(bob_uncleared)
 
-# Step 4
-alice_key = remove_garbage(alice_bases, bob_bases, alice_bits)
-bob_key = remove_garbage(alice_bases, bob_bases, bob_results)
+    # STEP 5: Bob declaration and Alice adjustment
+    print()
+    bob_adjusted = adjust_entangle(bob_uncleared, entangled_bits, None)
+    alice_adjusted = adjust_entangle(alice_uncleared, entangled_bits, bob_adjusted[len(bob_adjusted)-1])
+    print("Bob declares entanglement pair measured value: " + str(bob_adjusted[len(bob_adjusted)-1]))
+    print("Alice adjusted:")
+    print(alice_adjusted)
+    print("Bob adjusted:")
+    print(bob_adjusted)
 
-print("Bob filtered:")
-print(bob_key)
-print("Bob Key Length: " + str(len(bob_key)))
-print("Alice Key Length (should be same ^): " + str(len(alice_key)))
-print(alice_key)
-if bob_key == alice_key:
-    print("Shared Key:")
+    alice_key = clear_nones(alice_adjusted)
+    bob_key = clear_nones(bob_adjusted)
+
+    print()
+    print("FINAL:")
+    print("Alice Key:")
     print(alice_key)
-else:
-    print("Error")
+    print("Bob Key:")
+    print(bob_key)
+    print("Shared Key Length: " + str(len(bob_key)))
+    # print("Alice Key Length (should be same ^): " + str(len(alice_key)))
+
+bb84(n, entangled_sets[0])
+# print()
+# print()
+# bb84(n, entangled_sets[1])
